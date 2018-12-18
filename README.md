ec9624e973e5b32dbf4ba99558e9073d02463139
JDLong <jdlong1980@gmail.com>
2018-12-18 15:33:29 -0500

    updated all files

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Trivia-Game
+How much Disney trivia do you know?!? You have 20 seconds to answer each question to find out!
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,19 +1,20 @@
-var trivia = [
-    {
+var trivia = [{
         question: "What was the first character Walt Disney created?",
         answers: [
             { a1: "Mickey Mouse", value: false },
             { a2: "Goofy", value: false },
             { a3: "Donald Duck", value: false },
-            { a4: "Oswald the Lucky Rabbit", value: true, pic: "oswald.gif" }]
+            { a4: "Oswald the Lucky Rabbit", value: true, pic: "oswald.gif" }
+        ]
     },
     {
-        question: "Which princess is know for wearing a green dress?",
+        question: "Which princess is known for wearing a green dress?",
         answers: [
             { a1: "Cinderella", value: false },
             { a2: "Tiana", value: true, pic: "tiana.gif" },
             { a3: "Ariel", value: false },
-            { a4: "Snow White", value: false }]
+            { a4: "Snow White", value: false }
+        ]
     },
     {
         question: "Who gave the opening speech of Walt Disney World?",
@@ -21,7 +22,8 @@ var trivia = [
             { a1: "Roy Disney", value: true, pic: "roy_disney.gif" },
             { a2: "Walt Disney", value: false },
             { a3: "Julie Andrews", value: false },
-            { a4: "Michael Eisner", value: false }]
+            { a4: "Michael Eisner", value: false }
+        ]
     },
     {
         question: "What animal is found on Mary Poppin's umbrella?",
@@ -29,7 +31,8 @@ var trivia = [
             { a1: "Parrot", value: true, pic: "mary_umbrella.gif" },
             { a2: "Cat", value: false },
             { a3: "Goat", value: false },
-            { a4: "Frog", value: false }]
+            { a4: "Frog", value: false }
+        ]
     },
     {
         question: "Who wakes Sleeping Beauty with a kiss?",
@@ -37,7 +40,8 @@ var trivia = [
             { a1: "Prince Andrew", value: false },
             { a2: "Prince Philip", value: true, pic: "prince_phillip.gif" },
             { a3: "Prince Naveen", value: false },
-            { a4: "Prince Charming", value: false }]
+            { a4: "Prince Charming", value: false }
+        ]
     },
     {
         question: "'Let it Go' was made famous by what Disney movie?",
@@ -45,7 +49,8 @@ var trivia = [
             { a1: "Frozen", value: true, pic: "elsa.gif" },
             { a2: "101 Dalmatians", value: false },
             { a3: "Cinderella", value: false },
-            { a4: "Toy Story", value: false }]
+            { a4: "Toy Story", value: false }
+        ]
     },
     {
         question: "In 'The Lion King', what is Simba's mother's name?",
@@ -53,7 +58,8 @@ var trivia = [
             { a1: "Nala", value: false },
             { a2: "Sarafina", value: false },
             { a3: "Zazu", value: false },
-            { a4: "Sarabi", value: true, pic: "sarabi.gif" }]
+            { a4: "Sarabi", value: true, pic: "sarabi.gif" }
+        ]
     },
     {
         question: "How long was Genie from 'Aladdin' stuck in the lamp?",
@@ -61,7 +67,8 @@ var trivia = [
             { a1: "20,000 years", value: false },
             { a2: "15,000 years", value: false },
             { a3: "10,000 years", value: true, pic: "genie.gif" },
-            { a4: "1,000 years", value: false }]
+            { a4: "1,000 years", value: false }
+        ]
     },
     {
         question: "What causes Pinocchio's nose to grow?",
@@ -69,7 +76,8 @@ var trivia = [
             { a1: "Laughing", value: false },
             { a2: "Coughing", value: false },
             { a3: "Lying", value: true, pic: "pinocchio.gif" },
-            { a4: "Falling", value: false }]
+            { a4: "Falling", value: false }
+        ]
     },
     {
         question: "Which Disney movie features the song 'Two Worlds'?",
@@ -77,77 +85,140 @@ var trivia = [
             { a1: "The Little Mermaid", value: false },
             { a2: "The Princess and the Frog", value: false },
             { a3: "The Aristocats", value: false },
-            { a4: "Tarzan", value: true, pic: "tarzan.gif" }]
+            { a4: "Tarzan", value: true, pic: "tarzan.gif" }
+        ]
     }
 ];
 
-var questionNum, correct, incorrect, unanswered;
+var quesNum, correct, incorrect, unanswered;
+var quesSeconds = 20,
+    resultSeconds = 6;
+var intervalId, number;
+var mode; // 1 for question screen, 2 for result screen
 
-var nextQuestion = function () {
-    if (questionNum < trivia.length) {
-        $("#question").text(trivia[questionNum].question);
-        $("#a1").text(trivia[questionNum].answers[0].a1);
-        $("#a2").text(trivia[questionNum].answers[1].a2);
-        $("#a3").text(trivia[questionNum].answers[2].a3);
-        $("#a4").text(trivia[questionNum].answers[3].a4);
+function stopTimer() {
+    clearInterval(intervalId);
+};
 
+function runTimer() {
+    number--;
+    if (mode === 1) {
+        $("#remaining").text("Time remaining: " + number + " seconds");
+        if (number === 0) {
+            stopTimer();
+            displayResult(-2);
+        }
+    } else { // mode equals 2
+        if (number === 0) {
+            stopTimer();
+            quesNum++;
+            if (quesNum < trivia.length) {
+                nextQuestion();
+            } else {
+                displayStats();
+            };
+        };
+    }
+};
 
-        $("#start").hide();
-        $("#correct-text").hide();
-        $("#gif").hide();
-        $("#stats").hide();
+function nextQuestion() {
+    $("#question").text(trivia[quesNum].question);
+    $("#a1").text(trivia[quesNum].answers[0].a1);
+    $("#a2").text(trivia[quesNum].answers[1].a2);
+    $("#a3").text(trivia[quesNum].answers[2].a3);
+    $("#a4").text(trivia[quesNum].answers[3].a4);
 
-        $("#remaining").show();
-        $("#question").show();
-        $(".answer").show();
-    };
+    $("#start").hide();
+    $("#correct-text").hide();
+    $("#gif").hide();
+    $("#stats").hide();
+
+    $("#remaining").show();
+    $("#question").show();
+    $(".answer").show();
+
+    mode = 1;
+    number = quesSeconds;
+    $("#remaining").text("Time remaining: " + number + " seconds");
+    intervalId = setInterval(runTimer, 1000);
 }
 
-$("#start").on("click", function () {
-    questionNum = 0;
+function displayResult(result) {
+    var answer;
+    var src = "assets/images/"
+    for (i = 0; i < trivia[quesNum].answers.length; i++) {
+        if (trivia[quesNum].answers[i].value) {
+            switch (i) {
+                case 0:
+                    answer = trivia[quesNum].answers[i].a1;
+                    break;
+
+                case 1:
+                    answer = trivia[quesNum].answers[i].a2;
+                    break;
+
+                case 2:
+                    answer = trivia[quesNum].answers[i].a3;
+                    break;
+
+                case 3:
+                    answer = trivia[quesNum].answers[i].a4;
+            }
+            $("#correct-text").text("The correct answer is: " + answer);
+            $("#gif").attr("src", src + trivia[quesNum].answers[i].pic);
+        };
+    };
+
+    switch (result) {
+        case -1:
+            $("#question").text("Wrong!");
+            incorrect++;
+            break;
+        case -2:
+            $("#question").text("Unanswered!");
+            unanswered++;
+            break;
+        default:
+            correct++;
+            $("#question").text("Correct!");
+    }
+
+    if (result < 0) {
+        $("#correct-text").show();
+    };
+
+    $(".answer").hide();
+    $("#gif").show();
+
+    mode = 2;
+    number = resultSeconds;
+    intervalId = setInterval(runTimer, 1000);
+};
+
+var displayStats = function() {
+    $("#question").text("All done! Here's how you did:");
+    $("#correct-text").hide();
+    $("#gif").hide();
+    $("#stats").html("Correct answers: " + correct + "<br>Incorrect answers: " + incorrect + "<br>Unanswered: " + unanswered);
+    $("#stats").show();
+    $("#start").text("TRY AGAIN");
+    $("#start").show();
+}
+
+$("#start").on("click", function() {
+    quesNum = 0;
     correct = 0;
     incorrect = 0;
     unanswered = 0;
     nextQuestion();
 });
 
-$(".answer").on("click", function () {
+$(".answer").on("click", function() {
+    stopTimer();
     var index = parseInt($(this).data("index"));
-    var question = trivia[questionNum];
-    var src = "assets/images/"
-    $(".answer").hide();
-
-    if (question.answers[index].value) {
-        $("#question").text("Correct!");
-        $("#gif").attr("src", src + question.answers[index].pic);
+    if (trivia[quesNum].answers[index].value) {
+        displayResult(index);
     } else {
-        for (i = 0; i < question.answers.length; i++) {
-            if (question.answers[i].value) {
-                var answer;
-                switch (i) {
-                    case 0:
-                    answer = question.answers[i].a1;
-                    break;
-
-                    case 1:
-                    answer = question.answers[i].a2;
-                    break;
-
-                    case 2:
-                    answer = question.answers[i].a3;
-                    break;
-
-                    case 3:
-                    answer = question.answers[i].a4;
-                    break;                    
-                }
-                $("#question").text("Wrong!");
-                $("#correct-text").text("The correct answer is: " + answer);
-                $("#gif").attr("src", src + question.answers[i].pic);
-                $("#correct-text").show();
-            };
-        };
+        displayResult(-1);
     };
-
-    $("#gif").show();
 });
